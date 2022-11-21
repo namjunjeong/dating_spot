9708dbcb37a196a062b457f3860a5235e58eaaee
nj_jeong <playjnj@khu.ac.kr>
2022-11-22 02:13:32 +0900

    modified app.js

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 var express=require('express');
 var app=express();
-var kakaorest=require("./kakaorest")
+var search=require("./search")
 var bodyParser=require('body-parser');
+var requestIp=require("request-ip");
+var fs=require("fs");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.get('/',(req,res)=>{
-    res.send("main");
+    var userip=requestIp.getClientIp(req);
+    fs.writeFile('./ip',userip+'\n',{flag:'a+'},err=>{
+        if(err){
+            console.error(err)
+            return
+        }
+    })
+    res.render('app');
 })
 
-app.use('/rest',kakaorest);
+app.post('/',(req,res)=>{
+    res.redirect('./search');
+})
+app.use('/search',search);
 
 
-var server=app.listen(3000);
+var server=app.listen(3000,'0.0.0.0');
