8cd30c2afc0c90927f8113ddd342402467344ed1
nj_jeong <playjnj@khu.ac.kr>
2022-12-05 01:02:43 +0900

    Add recommending files

---
+++ src/Error.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Error extends React.Component{
+    render(){
+        return<div> error occured </div>
+    }
+}
+
+export default Error;
--- src/Process.jsx
+++ src/Process.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import axios from 'axios';
 var kakaokey="5819749ef21ee7d0aeff60d9a2178b99";
-const category_list={ //need to append more
-    "cafe" : ["카페","CE7"],//카페
+const category_list={ 
+    "cafe" : ["카페","CE7"],
     "restaurant" : ["음식점","FD6"],
     "tour" : ["관광명소","AT4"],
     "culture" : ["문화시설","CT1"],
@@ -30,32 +30,32 @@ class Process extends React.Component {
         }
     }
     componentDidMount(){
-        for(var tag of data){
-            this.callAPI(category_list[tag],info.x,info.y,info.range);
+        const search=async()=>{
+            for(var tag of data){
+                tag=category_list[tag];
+                tag[0]=encodeURI(tag[0]);
+                await axios({
+                    url :`https://dapi.kakao.com/v2/local/search/keyword.json?page=1&size=9&sort=accuracy&x=${info.x}&y=${info.y}&radius=${info.range}&category_group_code=${tag[1]}&query=${tag[0]}`,
+                    method : "GET",
+                    headers : {Authorization: `KakaoAK ${kakaokey}`}
+                }).then((response)=>{
+                    this.setState({
+                        output : [...this.state.output,response.data["documents"]],
+                        count : this.state.count+1
+                    });
+                }).catch((error)=>{
+                    console.log(error);
+                })
+            }
         }
+
+        search()
     }
 
 
-    callAPI = (tag,x_coor,y_coor,range)=>{
-        tag[0]=encodeURI(tag[0]);
-        axios({
-            url :`https://dapi.kakao.com/v2/local/search/keyword.json?page=1&size=9&sort=accuracy&x=${x_coor}&y=${y_coor}&radius=${range}&category_group_code=${tag[1]}&query=${tag[0]}`,
-            method : "GET",
-            headers : {Authorization: `KakaoAK ${kakaokey}`}
-        }).then((response)=>{
-            console.log("success");
-            this.setState({
-                output : [...this.state.output,response.data["documents"]],
-                count : this.state.count+1
-            });
-        }).catch((error)=>{
-            console.log(error);
-    })
-        
-    }
     render(){{/* <div>{this.state.output.map(value=><p>value</p>)}</div> */}
         return <div>
-            {this.state.count>1 ? <p>{this.state.output[0][0]["id"]}</p> : <p> loading </p>}
+            {this.state.count>0 ? this.state.output.map(value=><p>{value[0]["place_name"]}</p>) : <p> loading </p>}
         </div>
   }
 }
