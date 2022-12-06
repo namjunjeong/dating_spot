9d4123e89a55485e31b8670d5fd1265c7a3afe00
5un6h33 <pinedelta@icloud.com>
2022-12-07 02:16:48 +0900

    feat: user model 생성

---
+++ backend/src/models/user.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const UserSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    require: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    trim: true,
+    match: /.+\@.+\..+/,
+    unique: true,
+  },
+  username: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  created_at: { type: Date, default: Date.now },
+  updated_at: Date,
+});
+
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.static.login = async function (id, password) {
+  const user = await this.findOne({ id });
+
+  if (!user) throw Error("아이디가 올바르지 않습니다!");
+
+  const auth = await bcrypt.compare(password, user.password);
+
+  if (!auth) throw Error("비밀번호가 올바르지 않습니다!");
+
+  return user;
+};
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
