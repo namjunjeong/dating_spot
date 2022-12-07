276fc7acb0dd326d750f20936f06caeb2d987f8d
5un6h33 <pinedelta@icloud.com>
2022-12-07 17:23:27 +0900

    feat: 유저 탈퇴 기능 추가 및 세션 삽입

--- backend/src/api/routers/user.js
+++ backend/src/api/routers/user.js
@@ -4,7 +4,8 @@ import User from "../../models/user.js";
 const userRoute = express.Router();
 
 userRoute.get("/", async (req, res) => {
-  const user = await User.find();
+  if (!req.session.user) return res.send("로그인 해주세요").status(200);
+  const user = await User.findOne({ id: req.session.user.id });
   return res.json(user).status(200);
 });
 
@@ -16,11 +17,18 @@ userRoute.post("/", async (req, res) => {
       email: req.body.email,
       username: req.body.username,
     });
-    return res.sendStatus(201);
+    return res.send("회원가입 성공").status(201);
   } catch (error) {
     console.log(error);
-    res.sendStatus(500);
+    return res.send("회원가입 실패").status(400);
   }
+});
+
+userRoute.delete("/", async (req, res) => {
+  if (!req.session.user) return res.send("로그인 해주세요").status(200);
+  await User.deleteOne({ id: req.session.user.id });
+  req.session.destroy((err) => console.log("세션 삭제"));
+  return res.send("탈퇴 성공").status(200);
 });
 
 export default userRoute;
