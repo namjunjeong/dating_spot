5747f6c49cfb942f3d31e2d7cb66aeb9c9af9796
5un6h33 <pinedelta@icloud.com>
2022-12-07 17:21:57 +0900

    feat: 세션 추가

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,31 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import fileStore from "session-file-store";
 import helmet from "helmet";
 import mongoose from "mongoose";
 import config from "./config/index.js";
 import userRouter from "./api/routers/user.js";
+import authRouter from "./api/routers/auth.js";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(helmet());
+app.use(
+  session({
+    secret: config.secret,
+    resave: false,
+    saveUninitialized: true,
+    // store: new fileStore(),
+    cookie: { maxAge: 1000 * 60 * 5 },
+  })
+);
 app.use("/user", userRouter);
+app.use("/auth", authRouter);
 app.use((req, res, next) => res.status(404).send("Not Found"));
 
 app
