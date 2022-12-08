4420033283fc04a2bd10f7efaaa3e07bc176b891
nj_jeong <playjnj@khu.ac.kr>
2022-12-09 06:41:55 +0900

    Connect Router

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Start from './start/Start'
+import Result from './result/src/Result'
+import SelectCategory from './selectcategory/src/App'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById("content"));
-root.render(<App />);
+
+const root = ReactDOM.createRoot(document.getElementById('content'));
+root.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Start/>}/>
+            <Route path="/selectcategory" element={<SelectCategory/>}/>
+
+            <Route path="/result" element={<Result/>}/>
+        </Routes>
+    </BrowserRouter>
+);
