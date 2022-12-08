e048981b4dd1bf6fb0a4c39c8fb90299cc148dce
5un6h33 <pinedelta@icloud.com>
2022-12-09 06:27:46 +0900

    update: 시작화면

--- frontend/src/start/Start.jsx
+++ frontend/src/start/Start.jsx
@@ -1,22 +1,50 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import "./Start.css";
 
 const Start = () => {
+  const navigate = useNavigate();
+  const [flag, setFlag] = useState(false);
+  const RandomPick = (e) => {
+    let datacontainer;
+    (async () => {
+      await axios({
+        url: "http://127.0.0.1:3000/spot",
+        method: "get",
+      })
+        .then((response) => {
+          datacontainer = response.data;
+          let rand = Math.floor(Math.random() * (datacontainer.length - 1));
+          navigate("/result", { state: datacontainer[rand] });
+        })
+        .catch((error) => {
+          console.log(error);
+          setFlag(true);
+        });
+    })();
+  };
   return (
     <div className="container">
-      <link
-        href="./css/snow.css"
-        rel="stylesheet"
-        type="text/css"
-        media="all"
-      />
       <div className="word">
-        <h4>Shall_We_Date?</h4>
+        <span>Shall_We_Date?</span>
       </div>
-      <Link to="/selectcategory">
-        <button className="button">시작</button>
-      </Link>
+      <div className="buttonBox">
+        <div>
+          <Link to="/selectcategory">
+            <button className="btn startbt">두근두근 코스만들기</button>
+          </Link>
+        </div>
+        <div>
+          {flag === false ? (
+            <button className="btn randombt" onClick={RandomPick}>
+              두근두근 추천받기
+            </button>
+          ) : (
+            <span>에러 발생!</span>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
