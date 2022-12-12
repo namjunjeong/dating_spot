27a2289ec01ba4210ab9570ed5066f11fcee4e05
joonyjin <joonyjin@khu.ac.kr>
2022-12-12 16:40:33 +0900

    Error fixed

--- frontend/src/recommend/src_front/Home.js
+++ frontend/src/recommend/src_front/Home.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import { Route, Routes ,useLocation,useNavigate,useEffect} from 'react-router-dom';
 import Final from './Final';
@@ -21,7 +21,6 @@ function Home() {
         <center>
         <h1>카테고리를 선택해주세요</h1>
         <div className="flex-container">
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
         {list.map((a) => (
             <div key={a} style={{marginLeft:"2rem"  }}>
                 <Button type={a}/>
@@ -41,49 +40,3 @@ function Home() {
 }
 
 export default Home;
-
-=======
-import React from 'react';
-import { Route, Routes ,useLocation} from 'react-router-dom';
-import Final from './Final';
-import Category from './allinone';
-import './style.css';
-import arr from './Firststep';
-import Button from "./Button";
-function Home() {
-    const {state} = useLocation();
-    const list = [];
-    for (let key in state){
-        if (key != "x_coor" && key != "y_coor"){
-            list.push(key);
-        }
-    }
-    return (
-        <div>
-        {/* <Data /> 
-           만약 Data 렌더링 했으면, ***arr from ./Firststep 대신에*** 
-                                   *** useLocation()으로 state에 *** 받아준 후 쓰면 됨*/}
-        <h1>카테고리를 선택해주세요</h1>
-        <div className="flex-container">
-        {list.slice(2).map((a) => (
-            <Button cate={a} key={a}/>
-        ))}
-        </div>
-        <Routes>
-            <Route path= "/alc" element={<Category keys="alc" arr={state} />} />
-            <Route path="/cafe" element={<Category keys="cafe" arr={state}/>} />
-            <Route path="/culture" element={<Category keys="culture" arr={state}/>} />
-            <Route path="/pcroom" element={<Category keys="pcroom" arr={state}/>} />
-            <Route path="/place" element={<Category keys="place" arr={state}/>} />
-            <Route path="/restaurant" element={<Category keys="restaurant"arr={state}/>} />
-            <Route path="/shopping" element={<Category keys="shopping" arr={state}/>} />
-            <Route path="/themepark" element={<Category keys="themepark" arr={state}/>} />
-            <Route path="/final" element={<Final arr={state}/>} />
-        </Routes>
-        </div>
-    )
-}
-
-export default Home;
-
->>>>>>> 81aa9e31672920dbfbbb626d1620d9befcd724ff
