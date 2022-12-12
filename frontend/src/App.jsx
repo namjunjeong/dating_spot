91bffcfb2f92ccc4902bf54e6209cac911b52e9e
nj_jeong <playjnj@khu.ac.kr>
2022-12-13 06:18:32 +0900

    Change main component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,9 @@ import SelectCategory from "./selectcategory/SelectCategory";
 import Result from "./Result/Result.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { RecoilRoot } from "recoil";
-import Home from "./recommend/src_front/app_modify";
+import Recommend from "./recommend/src_front/app_modify";
+import Home from "./recommend/src_front/Home.js";
+import Final from "./recommend/src_front/Final";
 
 const App = () => {
   return (
@@ -13,7 +15,9 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Start />} />
           <Route path="/selectcategory" element={<SelectCategory />} />
-          <Route path="/recommend" element={<Home />} />
+          <Route path="/recommend" element={<Recommend />} />
+          <Route path="/home/*" element={<Home />} />
+          <Route path="/final" element={<Final />} />
           <Route path="/result" element={<Result />} />
         </Routes>
       </BrowserRouter>
